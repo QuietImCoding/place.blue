72d5ef5f916777dd31aa47ddb8ce683dc9aa212e
QuietImCoding <dmonte3210@gmail.com>
2024-12-04 16:42:08 -0800

    good normlal website haha

---
+++ frontend/scripts/atproto.js
@@ -0,0 +1,92 @@
+let domain, username, did, jwt, refresh;
+let authIndicator = document.getElementById("account")
+
+function getToken(provider, handle, password) { 
+    let response = fetch(provider + '/xrpc/com.atproto.server.createSession', {
+    method: "POST",
+    body: JSON.stringify({
+        identifier: handle,
+        password: password,
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    }).then((response) => response.json())
+    .then((json) => {
+        domain = provider;
+        username = json.handle;
+        did = json.did;
+        jwt = json.accessJwt;
+        refresh = json.refreshJwt
+        authIndicator.innerText = `(logged in as ${handle})`
+        authIndicator.style.color = "green"
+        setInterval(refreshToken, 60000)
+    });
+}
+
+function refreshToken() {
+    let response = fetch(domain + '/xrpc/com.atproto.server.refreshSession', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Authorization": "Bearer " + refresh
+        }
+        }).then((response) => response.json())
+        .then((json) => {
+            jwt = json.accessJwt;
+            refresh = json.refreshJwt
+            console.log("REFRESHED JWT")
+        });
+}
+
+function generateTID() {
+    return( 
+        Conversions.base32.encode(Date.now().toString())
+            .replaceAll("=", "")
+            .slice(-13)
+            .toLowerCase()
+    );
+}
+
+function publishPixel(xval, yval, cval) {
+    let response = fetch(domain + '/xrpc/com.atproto.repo.putRecord', {
+        method: "POST",
+        body: JSON.stringify({
+            repo: did,
+            collection: "blue.place.pixel",
+            rkey: generateTID(),
+            record: {
+                x: xval,
+                y: yval,
+                color: cval
+            }
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Authorization": "Bearer " + jwt
+        }
+        }).then((response) => response.json())
+        .then((json) => {
+            console.log(json)
+        });
+
+}
+
+
+
+document.getElementById("auth").addEventListener("click", () => {
+    getToken(
+        document.getElementById("provider").value,
+        document.getElementById('handle').value,
+        document.getElementById('password').value
+    )
+})
+
+let socketURI = 'wss://jetstream2.us-east.bsky.network/subscribe\?wantedCollections=blue.place.pixel'
+subscription = new WebSocket(socketURI);
+subscription.onmessage = e => {
+    let msgData = JSON.parse(e.data);
+    if ( msgData.type = "com" && msgData.kind == "commit") {
+        console.log(msgData);
+    }
+};
--- frontend/scripts/place.js
+++ frontend/scripts/place.js
@@ -3,14 +3,17 @@ canvas.height = 500;
 canvas.width = 500;
 const ctx = canvas.getContext("2d");
 var colorpreview = document.getElementById("colorpreview");
+let colorvalue = 0;
 let drawcolor, x, y; 
 
 
 Array.from(document.getElementsByClassName("colorbox")).forEach(element => {
     element.addEventListener("click", e => {
+        colorvalue = e.target.value;
         var fillcolor = window.getComputedStyle(e.target).backgroundColor;
         drawcolor = fillcolor;
         colorpreview.style.backgroundColor = fillcolor;
+        console.log(colorvalue)
     })
 });
 
@@ -22,9 +25,18 @@ function getCursorPosition(canvas, event) {
     return([x, y])
 }
 
+const COLORLIST = Array.from(
+    document.querySelectorAll('li.colorbox'))
+    .map( e => window.getComputedStyle(e).backgroundColor)
+
+function drawPixel(context, x, y, color) {
+
+}
+
 canvas.addEventListener('click', function(e) {
     [x, y] = getCursorPosition(canvas, e);
     console.log(x, y);
     ctx.fillStyle = drawcolor;
     ctx.fillRect(x, y, 10, 10);
+    publishPixel(x, y, colorvalue);
 })
