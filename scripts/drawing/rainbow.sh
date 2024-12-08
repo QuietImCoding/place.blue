1f6b751c5421ed68da28c027942c6673418771ab
QuietImCoding <dmonte3210@gmail.com>
2024-12-07 19:06:48 -0800

    added example rainbow drawing script--- in BASH i know how much you all love BASH!!

---
+++ scripts/drawing/rainbow.sh
@@ -0,0 +1,27 @@
+function draw_pixel() {
+    x=$1
+    y=$2
+    c=$3
+    sleep 0.3
+    cdate="$(date -Iseconds)"
+    cat example.json | sed -e "s/COLOR/$((c % 6 + 8))/g" -e "s/DATE/${cdate:0:19}.00z/g" -e "s/XCOR/$x/g" -e "s/YCOR/$((499 - y))/g" > record.json
+    goat record create -n record.json &    
+}
+
+for iter in `seq 0 100`; do
+    x=$iter
+    for y in `seq 0 $iter`; do
+	draw_pixel $x $y $iter
+    done
+    for x in `seq $iter 0`; do
+	draw_pixel $x $y $iter
+    done
+    wait
+done
+
+
+# staic color
+#cat example.json | sed -e "s/COLOR/12/g" -e "s/DATE/${cdate:0:19}.00z/g" -e "s/XCOR/$x/g" -e "s/YCOR/$((500 - y))/g" | tee record.json
+# more rainbowy
+
+#
